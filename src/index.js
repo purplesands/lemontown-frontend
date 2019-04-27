0f866e0c21de2b6fbe0c7d7dfd930289afd4d571
purplesands <mullenjoe13@gmail.com>
2019-04-26 21:41:26 -0400

    try

--- src/index.js
+++ src/index.js
@@ -14,9 +14,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 
-
-
-
 ReactDOM.render(
   <ActionCableProvider url={'ws://lemon-town-api.herokuapp.com/cable'}>
     <Provider store={store}>
