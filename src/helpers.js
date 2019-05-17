72b35dd7f556b19725987091fbd1433ef06d0ac5
purplesands <mullenjoe13@gmail.com>
2019-05-17 12:31:42 -0400

    change url

--- src/helpers.js
+++ src/helpers.js
@@ -4,5 +4,5 @@
 export const url= 'http://lemon-town-api.herokuapp.com'
 export const ws = 'ws://lemon-town-api.herokuapp.com'
 
-export const wordsApiKey = ENV['WORDS_API_KEY']
-// export const wordsApiKey = process.env.REACT_APP_WORDS_API_KEY
+// export const wordsApiKey = ENV['WORDS_API_KEY']
+export const wordsApiKey = process.env.REACT_APP_WORDS_API_KEY
