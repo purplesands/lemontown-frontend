c7d0a79b0ad7ff63d16e1fcfa8c65d161324d08b
purplesands <mullenjoe13@gmail.com>
2019-05-17 12:55:42 -0400

    secure cable

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,7 @@
 // export const ws = 'ws://localhost:3000'
 
 export const url= 'https://lemon-town-api.herokuapp.com'
-export const ws = 'ws://lemon-town-api.herokuapp.com'
+export const ws = 'wss://lemon-town-api.herokuapp.com'
 
 // export const wordsApiKey = ENV['WORDS_API_KEY']
 export const wordsApiKey = process.env.REACT_APP_WORDS_API_KEY
